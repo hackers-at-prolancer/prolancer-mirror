7a81cf2ee930f2b8cde1c3415d1f1f1e07a80821
swati <khusi.swati@gmail.com>
2016-04-30 23:54:00 -0700

    V-1.0

---
+++ prolancer/config/initializers/assets.rb
@@ -0,0 +1,22 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( flexslider.css )
+Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+
+Rails.application.config.assets.precompile += %w( jquery-1.10.2.js )
+Rails.application.config.assets.precompile += %w( bootstrap.js )
+Rails.application.config.assets.precompile += %w( jquery.flexslider.js )
+Rails.application.config.assets.precompile += %w( scrollReveal.js )
+Rails.application.config.assets.precompile += %w( jquery.easing.min.js )
+Rails.application.config.assets.precompile += %w( custom.js )
